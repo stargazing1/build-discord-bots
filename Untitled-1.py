0b657b2952c1fb718406df5013dd10aa6c51322a
Stargazing Technologies <60585007+stargazing1@users.noreply.github.com>
2022-06-09 17:20:05 -0700

    A few memes
    memes ig

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+for x in range(0,20): 
+    print (hex(x))
+
+sus = ["amogus", "sus", "imposter"]
+for x in sus: 
+    print(x)
+    if x == "imposter":
+        break
+
+amongus = ["sus", "bunga", "bingqilin"]
+for x in amongus:
+  print (x)
+
+  for x in sus: 
+    if x == "imposter":
+        break
+    print(x)
